55015468c5000a7112e9d89084d1042ebb238767
unknown <ycc525@nyu.edu>
2020-09-16 20:14:52 +0800

    final

---
+++ projects/coding-foundation/README.md
@@ -0,0 +1,5 @@
+### About the assignment
+###### I did encounter some difficulties during coding but fortunately it turned out to be great!
+* haven't been using JavaScript for so long and forgot so many syntaxes
+* didn't think of a way to clear the existing boxes out
+* Leon told me a trick that I never thought of when using innerHTML and that is so convenient!
--- projects/coding-foundation/script.js
+++ projects/coding-foundation/script.js
@@ -1,8 +1,15 @@
-var num=document.getElementById('valueNumber');
-var slider=document.getElementById('imageNumber');
-
-num.innerHTML=slider.value;
-
-slider.oninput = function() {
-  num.innerHTML = this.value;
+var slider = document.getElementById('boxNumber');
+var num = document.getElementById('text');
+var flex =document.getElementById('flex');
+// call this function everytime when the changes of input of the slider is detected
+slider.addEventListener("input", function(){
+  num.innerHTML= "there are " + this.value+ " grids";
+  var k= this.value;// declare a variable for the input
+  console.log(k);
+  flex.innerHTML= " ";// clear the exsiting flexboxes out of the div
+  for (i=0; i<k; i++){
+  var box = document.createElement('div'); //creating boxes
+  box.setAttribute('class', 'boxes');//set a class for styling in css
+  flex.appendChild(box);//and put the flexboxes inside the div
 }
+});
