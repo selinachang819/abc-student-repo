b419ec65114183fbae4cbdcd36dd1c5e9833a66f
unknown <ycc525@nyu.edu>
2020-09-23 21:53:57 +0800

    mini project

--- projects/mini-project-I/script.js
+++ projects/mini-project-I/script.js
@@ -1,6 +1,6 @@
 let slider = document.getElementById('slider');
-let images1 =[document.getElementById('a1'), document.getElementById('a3'), document.getElementById('c2')];
-let images2 =[document.getElementById('a2'), document.getElementById('c1'), document.getElementById('c3')];
+let images1 =[document.getElementById('a1'), document.getElementById('a3'), document.getElementById('c1')];
+let images2 =[document.getElementById('a2'), document.getElementById('c2'), document.getElementById('c3')];
 
 slider.addEventListener("input",function(){
   console.log(slider.value);
@@ -11,6 +11,6 @@ slider.addEventListener("input",function(){
   }
   for (i=0; i<images2.length; i ++){
     images2[i].style.transform="rotate("+(slider.value-135)*-3+"deg)";
-    images2[i].style.left=-(slider.value-135)*-3+ "px";
+    images2[i].style.left=(slider.value-135)*3+ "px";
   }
 })
