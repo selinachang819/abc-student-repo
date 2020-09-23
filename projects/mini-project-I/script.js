e57abda61f7195c81f976cbf8e009f55c881bc43
unknown <ycc525@nyu.edu>
2020-09-23 21:58:06 +0800

    mini project

--- projects/mini-project-I/script.js
+++ projects/mini-project-I/script.js
@@ -1,16 +1,17 @@
+// select items
 let slider = document.getElementById('slider');
-let images1 =[document.getElementById('a1'), document.getElementById('a3'), document.getElementById('c2')];
-let images2 =[document.getElementById('a2'), document.getElementById('c1'), document.getElementById('c3')];
+let images1 =[document.getElementById('a1'), document.getElementById('a3'), document.getElementById('c1')];
+let images2 =[document.getElementById('a2'), document.getElementById('c2'), document.getElementById('c3')];
 
 slider.addEventListener("input",function(){
   console.log(slider.value);
-
+//apply changes to each one in the array
   for (i=0; i<images1.length; i ++){
     images1[i].style.transform="rotate("+(slider.value-135)*3+"deg)";
     images1[i].style.left=-(slider.value-135)*3+ "px";
   }
   for (i=0; i<images2.length; i ++){
     images2[i].style.transform="rotate("+(slider.value-135)*-3+"deg)";
-    images2[i].style.left=-(slider.value-135)*-3+ "px";
+    images2[i].style.left=(slider.value-135)*3+ "px";
   }
 })
